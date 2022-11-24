4e89b87ba56dd5b41872245610b01d502c55e3fa
jagadeeshnethinti <109734275+jagadeeshnethinti@users.noreply.github.com>
2022-11-24 19:41:33 +0530

    Add files via upload

---
+++ arthematical.py
@@ -0,0 +1,14 @@
+a=int(input())
+b=int(input())
+c=a+b
+print('addition ',c)
+c=a-b
+print('substraction',c)
+c=a*b
+print('multiplication',c)
+c=a/b
+print('division',c)
+c=a**b
+print('a power b',c)
+c=a//b
+print('floor divide',c)
